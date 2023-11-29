d9046530448ea888d94a23b108c0c0f5243335f0
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-11-29 23:05:44 +0600

    first commit

---
+++ Mod 11/Code/update.py
@@ -0,0 +1,28 @@
+import mysql.connector
+db_name = "python_test_db"
+
+mydbconnection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "password",
+    database = db_name
+
+)
+
+
+
+
+mycursor = mydbconnection.cursor()
+
+sqlquery = """
+            UPDATE Student 
+            SET Name = 'KKP'
+            WHERE Name = 'Kawshik Kumar Paul'
+
+"""
+
+mycursor.execute(sqlquery)
+mydbconnection.commit()
+print("Table updated successfully")
+
+
